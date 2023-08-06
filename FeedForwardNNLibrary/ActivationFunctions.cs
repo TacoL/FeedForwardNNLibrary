fc60be6da74939046c4aadd10d7ebaf3f639fd2c
TacoL <34144367+TacoL@users.noreply.github.com>
2023-08-06 12:52:33 -0400

    Finished Import Model

--- FeedForwardNNLibrary/ActivationFunctions.cs
+++ FeedForwardNNLibrary/ActivationFunctions.cs
@@ -15,5 +15,19 @@ namespace FeedForwardNNLibrary
         public static ActivationFunctions ReLu { get; private set; } = new ActivationFunctions("ReLu");
         public static ActivationFunctions Softmax { get; private set; } = new ActivationFunctions("Softmax");
         public static ActivationFunctions None { get; private set; } = new ActivationFunctions("None");
+
+        public static ActivationFunctions ConvertFromString(string str)
+        {
+            if (str == null) throw new ArgumentNullException("str");
+
+            switch(str)
+            {
+                case "Tanh": return ActivationFunctions.Tanh;
+                case "ReLu": return ActivationFunctions.ReLu;
+                case "Softmax": return ActivationFunctions.Softmax;
+                case "None": return ActivationFunctions.None;
+                default: throw new Exception("Not a valid activation function");
+            }
+        }
     }
 }
--- FeedForwardNNLibrary/Network.cs
+++ FeedForwardNNLibrary/Network.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 
@@ -219,43 +219,42 @@ namespace FeedForwardNNLibrary
             settings.Indent = true;
             settings.IndentChars = "\t";
 
-            using (XmlWriter writer = XmlWriter.Create(filePath, settings))
+            using XmlWriter writer = XmlWriter.Create(filePath, settings);
+            writer.WriteStartElement("network");
+
+            writer.WriteElementString("numInputs", _numInputs.ToString());
+            writer.WriteElementString("learningRate", _learningRate.ToString());
+            writer.WriteElementString("momentumScalar", _momentumScalar.ToString());
+            writer.WriteElementString("batchSize", _batchSize.ToString());
+
+            writer.WriteStartElement("layers");
+            for (int layerIdx = 1; layerIdx < layers.Count; layerIdx++)
             {
-                writer.WriteStartElement("network");
-
-                writer.WriteElementString("numInputs", _numInputs.ToString());
-                writer.WriteElementString("learningRate", _learningRate.ToString());
-                writer.WriteElementString("momentumScalar", _momentumScalar.ToString());
-                writer.WriteElementString("batchSize", _batchSize.ToString());
-
-                writer.WriteStartElement("layers");
-                for (int layerIdx = 1; layerIdx < layers.Count; layerIdx++)
+                writer.WriteStartElement("layer" + layerIdx.ToString());
+                writer.WriteElementString("NumNeurons", layers[layerIdx].Count.ToString());
+                writer.WriteElementString("ActivationFunction", layers[layerIdx][0]._activationFunction.activation);
+                writer.WriteStartElement("neurons");
+                for (int neuronIdx = 0; neuronIdx < layers[layerIdx].Count; neuronIdx++)
                 {
-                    writer.WriteStartElement("layer" + layerIdx.ToString());
-                    writer.WriteElementString("ActivationFunction", layers[layerIdx][0]._activationFunction.activation);
-                    writer.WriteStartElement("neurons");
-                    for (int neuronIdx = 0; neuronIdx < layers[layerIdx].Count; neuronIdx++)
+                    writer.WriteStartElement("neuron" + neuronIdx.ToString());
+
+                    Neuron neuron = layers[layerIdx][neuronIdx];
+                    writer.WriteStartElement("weights");
+                    for (int weightIdx = 0; weightIdx < neuron.weights.Length; weightIdx++)
                     {
-                        writer.WriteStartElement("neuron" + neuronIdx.ToString());
-
-                        Neuron neuron = layers[layerIdx][neuronIdx];
-                        writer.WriteStartElement("weights");
-                        for (int weightIdx = 0; weightIdx < neuron.weights.Length; weightIdx++)
-                        {
-                            writer.WriteElementString("weight" + weightIdx.ToString(), neuron.weights[weightIdx].ToString());
-                        }
-                        writer.WriteEndElement(); // end weights
-                        writer.WriteElementString("bias", neuron.bias.ToString());
-                        writer.WriteEndElement(); // end neuron[idx]
+                        writer.WriteElementString("weight" + weightIdx.ToString(), neuron.weights[weightIdx].ToString());
                     }
-                    writer.WriteEndElement(); // end neurons
-                    writer.WriteEndElement(); // end layer[idx]
+                    writer.WriteEndElement(); // end weights
+                    writer.WriteElementString("bias", neuron.bias.ToString());
+                    writer.WriteEndElement(); // end neuron[idx]
                 }
-
-                writer.WriteEndElement(); // end layers
-                writer.WriteEndElement(); // end network
-                writer.Close();
+                writer.WriteEndElement(); // end neurons
+                writer.WriteEndElement(); // end layer[idx]
             }
+
+            writer.WriteEndElement(); // end layers
+            writer.WriteEndElement(); // end network
+            writer.Close();
         }
 
         /// <summary>
@@ -265,11 +264,55 @@ namespace FeedForwardNNLibrary
         /// <returns></returns>
         public static Network ImportModel(string filePath)
         {
-            using XmlReader xmlReader = XmlReader.Create(filePath);
 
-            xmlReader.Read();
+            int numInputs = 0;
+            double learningRate = 0;
+            double momentumScalar = 0;
+            int batchSize = 0;
+            using XmlReader reader = XmlReader.Create(filePath);
+            Network? network = null;
 
-            return new Network(0, 0, 0, 0);
+            int numNeurons = 0;
+            Neuron? currentNeuron = null;
+
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    switch (reader.Name)
+                    {
+                        case "numInputs": numInputs = reader.ReadElementContentAsInt(); break;
+                        case "learningRate": learningRate = reader.ReadElementContentAsDouble(); break;
+                        case "momentumScalar": momentumScalar = reader.ReadElementContentAsDouble(); break;
+                        case "batchSize":
+                            batchSize = reader.ReadElementContentAsInt();
+                            network = new Network(numInputs, learningRate, momentumScalar, batchSize);
+                            break;
+                        case "NumNeurons": numNeurons = reader.ReadElementContentAsInt(); break;
+                        case "ActivationFunction": network.AddLayer(numNeurons, ActivationFunctions.ConvertFromString(reader.ReadElementContentAsString())); break;
+                        case "bias": currentNeuron.bias = reader.ReadElementContentAsDouble(); break;
+                        default: break;
+                    }
+
+                    if (reader.Name.Contains("neuron") && reader.Name != "neurons")
+                    {
+                        int neuronIdx = int.Parse(reader.Name.Substring(6, 1));
+                        currentNeuron = network.layers[network.layers.Count - 1][neuronIdx];
+                    }
+                    else if (reader.Name.Contains("weight") && reader.Name != "weights")
+                    {
+                        int weightIdx = int.Parse(reader.Name.Substring(6, 1));
+                        currentNeuron.weights[weightIdx] = reader.ReadElementContentAsDouble();
+                    }
+                }
+                else if (reader.NodeType == XmlNodeType.EndElement)
+                {
+
+                }
+            }
+
+            if (network == null) { throw new Exception("Network doesn't exist"); }
+            return network;
         }
         #endregion
     }
